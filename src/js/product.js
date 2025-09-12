c4d1f6320bb551249df8fa266511ddb15ba73439
Beduman <behunz77@gmail.com>
2025-09-11 21:40:51 -0700

    week 2 team

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,13 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
+const productId = getParam('product');
 const dataSource = new ProductData("tents");
+
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
