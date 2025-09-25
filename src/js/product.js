aca65a2a5d526b6552e289604c72df9e5d50b027
Beduman <behunz77@gmail.com>
2025-09-24 22:06:14 -0700

    vite matches

--- src/js/product.js
+++ src/js/product.js
@@ -1,25 +1,25 @@
-import { setLocalStorage } from "./utils.mjs";
-import { getParam } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
+import { setLocalStorage } from './utils.mjs';
+import { getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
 const productId = getParam('product');
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
 
 const product = new ProductDetails(productId, dataSource);
 product.init();
 
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  setLocalStorage('so-cart', product);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  const products = await dataSource.findProductById(e.target.dataset.id);
+  addProductToCart(products);
 }
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
